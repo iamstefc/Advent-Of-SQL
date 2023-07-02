55713569baa32c966e857897eccb5822e4898ef6
iamstefc <chiarellastef@gmail.com>
2023-07-01 20:36:44 -0400

    3 day average after getting sum each day

--- interview-data-analytics/financialservices-large-A/transactions_rolling_average.sql
+++ interview-data-analytics/financialservices-large-A/transactions_rolling_average.sql
@@ -1,13 +1,42 @@
 /* Using this dataset, show the SQL query to find the rolling 3 day average transaction amount for each day in January 2021. */
 
-SELECT
-  DATE_TRUNC('day', transaction_time) AS transaction_date
-  , AVG(transaction_amount) AS average_transaction_amount
+WITH daily_transaction_sum AS (
+    SELECT
+        DATE_TRUNC('day', transaction_time) AS transaction_date
+        , SUM(transaction_amount) AS total_transaction_amount
+    FROM
+        transactions
+    WHERE
+        DATE_TRUNC('month', transaction_time) = DATE('2021-01-01')
+    GROUP BY
+        DATE_TRUNC('day', transaction_time)
+    ORDER BY
+        transaction_date
+
+), 
+
+rolling_average AS (
+    SELECT
+        t1.transaction_date
+        , t1.total_transaction_amount
+        , (t1.total_transaction_amount +
+        COALESCE(t2.total_transaction_amount, 0) +
+        COALESCE(t3.total_transaction_amount, 0)) / 3 AS rolling_average
+    FROM
+        daily_transaction_sum t1
+    LEFT JOIN
+        daily_transaction_sum t2 
+        ON t2.transaction_date = (t1.transaction_date - INTERVAL '1 day')
+    LEFT JOIN
+        daily_transaction_sum t3 
+        ON t3.transaction_date = (t1.transaction_date - INTERVAL '2 days')
+    ORDER BY
+    t1.transaction_date
+)
+
+SELECT 
+    transaction_date
+    , total_transaction_amount
+    , rolling_average
 FROM
-  transactions
-WHERE
-  DATE_TRUNC('month', transaction_time) = DATE('2021-01-01')
-GROUP BY
-  DATE_TRUNC('day', transaction_time)
-ORDER BY
-  transaction_date;
+    rolling_average
